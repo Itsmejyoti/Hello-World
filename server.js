47128a80d1f9693367983eb62342780539f75e8b
Itsmejyoti <waykar.jyoti@gmail.com>
2016-10-20 12:12:03 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var pg = require('pg');
+
+var app = express();
+
+app.set('port', process.env.PORT || 5000);
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
